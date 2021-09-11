06ddea9e9e49626ddaa27aa7381b82f8dea7f776
Alexandre Maurell <maurelllopes@mac.com>
2021-09-11 02:19:44 -0300

    Projeto Criado

---
+++ src/test/java/com/maurelllopes/projeto_loja/ProjetoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.projeto_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
