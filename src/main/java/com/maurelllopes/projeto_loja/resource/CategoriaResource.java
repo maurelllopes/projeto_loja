a7595704c812cbf831a83c6275b09fb82a8b97a7
Alexandre Maurell <maurelllopes@mac.com>
2021-09-11 03:52:05 -0300

    REST FUNCINANDO

---
+++ src/main/java/com/maurelllopes/projeto_loja/resource/CategoriaResource.java
@@ -0,0 +1,21 @@
+package com.maurelllopes.projeto_loja.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST FUNCIONANDO";
+	}
+	
+	
+	
+	
+
+}
